11cbd86e037688a7c9bdeca1768ebc637432356d
huanghaiou <huanghaiou@jrq.com>
2017-09-29 14:33:09 +0800

    aa

--- Apps/Common/Conf/db.php
+++ Apps/Common/Conf/db.php
@@ -6,14 +6,14 @@
  * Time: 16:55
  */
 return array(
-    'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '10.11.10.83', // 服务器地址
-    'DB_NAME'   => 'kktv_v1', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'sd-9898w', // 密码
-    'DB_PORT'   => 3306, // 端口
-    'DB_PREFIX' => 's_', // 数据库表前缀
-    'DB_CHARSET'=> 'utf8', // 字符集
-    'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
+    'DB_TYPE'    => 'mysql', // 数据库类型
+    'DB_HOST'    => '127.0.0.1', // 服务器地址
+    'DB_NAME'    => 'mangguo', // 数据库名
+    'DB_USER'    => 'root', // 用户名
+    'DB_PWD'     => 'root123', // 密码
+    'DB_PORT'    => 3306, // 端口
+    'DB_PREFIX'  => 's_', // 数据库表前缀
+    'DB_CHARSET' => 'utf8', // 字符集
+    'DB_DEBUG'   => true, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
 
 );
